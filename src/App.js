1eb598352feacfbb82051e05e3fb4617e3b04c72
admin <admin@NT-MacOS.local>
2021-05-16 14:11:33 +0700

    update our menu-item to navigate withRouter HOC-higher order component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./pages/hompage/homepage.component";
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>
+      HATSPAGE
+    </h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+    <Switch >
+      <Route exact path='/' component={HomePage}/>
+      <Route exact path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import './menu-item.style.scss';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem = ({ title, imageUrl, id, size }) => (
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div
     className={`menu-item ${size ? size : ''}`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
   > 
     <div 
     className='background-image'
@@ -17,4 +19,4 @@ const MenuItem = ({ title, imageUrl, id, size }) => (
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
